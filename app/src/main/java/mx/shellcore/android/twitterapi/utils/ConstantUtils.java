0b3dbb0b82d17748cff8303f8ea99d73bff40f04
Cesar Morales Garduño <shell.one.core@gmail.com>
2015-09-26 12:32:17 -0500

    Se agregó constantes de la aplicación.

---
+++ app/src/main/java/mx/shellcore/android/twitterapi/utils/ConstantUtils.java
@@ -0,0 +1,13 @@
+package mx.shellcore.android.twitterapi.utils;
+
+public class ConstantUtils {
+
+    public static final String URL_ROOT_TWITTER_API = "https://api.twitter.com";
+    public static final String URL_SEARCH = URL_ROOT_TWITTER_API + "/1.1/search/tweets.json?q=";
+    public static final String URL_AUTHENTICATION = URL_ROOT_TWITTER_API + "/oauth2/token";
+
+    public static final String CONSUMER_KEY = "jNGMc4ZcvKUiVdsnwYz0p9uPS";
+    public static final String CONSUMER_SECRET = "SfNmzvWAlvLMKrdMeQUjeYYfj6TQVFp3rLM0n0O0dO0OPnpOeD";
+
+    public static final String MEJORANDROID_TERM = "mejorandroid";
+}
